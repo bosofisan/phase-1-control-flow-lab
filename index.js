5cc1c8c9804b791d4a078060713d1d6d4e058155
lulu osofisan <boluosofisan@gmail.com>
2023-11-03 21:58:26 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result;
+  if (someValue <= 400){
+    result = 'This one is on me!'
+  }
+  else if (someValue > 400 && someValue < 2000) {
+    result = 'That will be twenty bucks.';
+  }
+  else if (someValue >= 2000 && someValue <=2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  let result;
+   if (someCity === 'NYC'){
+    result = 'Ok, sounds good.'
+   }
+   else {
+    result = 'No go.'
+   }
+   return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+  let response
+  switch (Tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+      case 'not as generous':
+        response = 'Thank you.'
+        break;
+        default:
+          response = 'Bye.'
+  }
+  return response
 }
